a059b1aad62fdd69941d58673bd29dec9f181587
Andrew Ittner <aji@rhymingpanda.com>
2019-02-27 15:06:48 -0800

    Add Zurb Foundation library
    - Switch to SASS stylesheet.
    - Update basics, skills, work sections to use XY Grid.
    - Move close to the compact theme.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const Handlebars = require("handlebars");
 
 function render(resume) {
-	const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+    console.info("[render]", "starting");
+    // const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+    // console.info("[render]", "css", css);
+    let cssCompiled;
+    try {
+        cssCompiled = buildStyles();
+    } catch(exc) {
+        console.err(exc);
+    }
+    // console.info("[render]", "cssCompiled", cssCompiled);
 	const tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
 	const partialsDir = path.join(__dirname, 'partials');
 	const filenames = fs.readdirSync(partialsDir);
@@ -20,9 +29,22 @@ function render(resume) {
 	  Handlebars.registerPartial(name, template);
 	});
 	return Handlebars.compile(tpl)({
-		css: css,
+		css: cssCompiled,
 		resume: resume
 	});
+}
+
+function buildStyles() {
+    // console.info("[buildStyles]", "starting");
+    const sass = require("node-sass");
+ 
+    const sassSrcPath = path.join(__dirname, 'sass', 'main.scss');
+    const foundationSCSSPath = path.join(__dirname, 'node_modules', 'foundation-sites', 'scss');
+    // console.info("sassSrcPath", sassSrcPath);
+    const resultRender = sass.renderSync({file: sassSrcPath, includePaths: [foundationSCSSPath]});
+    const resultCSS = resultRender.css.toString();
+    // console.info("[buildStyles]", "resultCSS", resultCSS);
+    return resultCSS;
 }
 
 module.exports = {
