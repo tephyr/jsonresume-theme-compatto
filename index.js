81cf5e14e8a0f41c3f2b22faabfdffffc6e4adf4
Andrew Ittner <aji@rhymingpanda.com>
2019-03-01 14:56:54 -0800

    Add date formatter with date-fns library

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const Handlebars = require("handlebars");
+const dateFnParse = require('date-fns/parse');
+const dateFnFormat = require('date-fns/format');
 
 function render(resume) {
-    console.info("[render]", "starting");
+    console.info("[render]", "starting", arguments.length);
     // const css = fs.readFileSync(__dirname + "/style.css", "utf-8");
     // console.info("[render]", "css", css);
     let cssCompiled;
@@ -35,17 +37,33 @@ function render(resume) {
 }
 
 function buildStyles() {
-    // console.info("[buildStyles]", "starting");
     const sass = require("node-sass");
- 
     const sassSrcPath = path.join(__dirname, 'sass', 'main.scss');
     const foundationSCSSPath = path.join(__dirname, 'node_modules', 'foundation-sites', 'scss');
-    // console.info("sassSrcPath", sassSrcPath);
     const resultRender = sass.renderSync({file: sassSrcPath, includePaths: [foundationSCSSPath]});
-    const resultCSS = resultRender.css.toString();
-    // console.info("[buildStyles]", "resultCSS", resultCSS);
-    return resultCSS;
+    return resultRender.css.toString();
 }
+
+Handlebars.registerHelper("standardizeDate", function(options) {
+    const resumeDate = options.fn(this);
+    if (!resumeDate) {
+        return 'Present';
+    }
+
+    const regexYearMonth = /\d{4}-\d{2}/;
+    let newDate;
+    if (resumeDate.length === 4) {
+        // assume year
+        newDate = resumeDate;
+    } else if (regexYearMonth.test(resumeDate)) {
+        newDate = dateFnFormat(dateFnParse(`${resumeDate}-01`), 'MMM YYYY');
+    } else {
+        console.info('[standardizeDate]', 'fell through', resumeDate);
+    }
+
+    // console.info('newDate: ' + newDate);
+    return newDate;
+});
 
 module.exports = {
 	render: render
